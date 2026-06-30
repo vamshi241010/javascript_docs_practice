12e919f2bbdb65ef0bd564819d42081b4534b668
vamshi <vamshi242010@gmail.com>
2026-07-01 00:04:04 +0530

    Implement async withdraw function with balance update

---
+++ sample1.js
@@ -0,0 +1,16 @@
+let balance = 100;
+
+async function withdraw(amount) {
+  const current = balance;
+  await new Promise(resolve => setTimeout(resolve, Math.random() * 100));
+  balance = current - amount;
+}
+
+Promise.all([
+  withdraw(30),
+  withdraw(50)
+]).then(() => {
+  console.log(balance); // Sometimes 50, sometimes 20
+});
+ 
+ 
