65e08ce36232974a2543c9fc49bbe25d7389a164
vamshi <vamshi242010@gmail.com>
2026-06-30 13:13:34 +0530

    Add readline interface for user input

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // console.log("hello i am vamshi");
 // alert("I'm JavaScript!");
 
-
-let name = "vamshi"
-console.log("Hey"+name)
+const readline = require("readline");
+const r1 = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+});
+r1.question("Enter Your name: ",(name)=>{
+console.log(name)
+r1.close()
+});
