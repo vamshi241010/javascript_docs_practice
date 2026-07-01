9184dfe0015c7cd24ea183d9e0274fe6b972e0ba
vamshi <vamshi242010@gmail.com>
2026-07-01 14:13:46 +0530

    Refactor test.py to include divide function
    Comment out print statements and add divide function.

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-print(my name)
-print(helllo mawa my name is vamshi)
+# print(my name)
+# print(helllo mawa my name is vamshi)
+def divide(a, b):
+    return a / b
+
+result = divide(10, 0)
+print(result)
